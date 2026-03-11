3c3494fa97184e4a6e9e0007b343b8fca644fb7c
kothakapusairam <kothakapusairam@gmail.com>
2026-03-11 11:49:22 +0530

    Add files via upload

---
+++ Balance.java
@@ -0,0 +1,14 @@
+package MyPack;
+public class Balance{
+public String name;
+public double bal;
+public Balance(String name, double bal) {
+this.name=name;
+this.bal=bal;
+}
+public void show(){
+if(bal<0)
+System.out.print("--> ");
+System.out.print(name+":$"+bal);
+}
+}
---
+++ TestBalance.java
@@ -0,0 +1,8 @@
+import MyPack.Balance;
+class TestBalance {
+public static void main(String args[]) {
+
+Balance test=new Balance("Harisa",676767);
+test.show();
+}
+}
